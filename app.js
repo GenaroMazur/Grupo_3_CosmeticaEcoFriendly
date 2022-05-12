34e0bbfd08404761d5b91526d9764d8c95c0df24
Genaro_Mazur <genaromazur03@gmail.com>
2022-05-12 20:45:44 -0300

    [mod : app.js]

--- app.js
+++ app.js
@@ -13,3 +13,15 @@ app.use(express.static(directory))
 app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname,"/views/index.html"))
 })
+app.get("/login",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/login.html"))
+})
+app.get("/registro",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/register.html"))
+})
+app.get("/CarritoDeCompras",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/productCard.html"))
+})
+app.get("/DetalleDeProducto",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/productDetail.html"))
+})
