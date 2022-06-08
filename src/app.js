d398a0abb072ab1c4163d3e130884cd1ee9fb5dd
GenaroMazur <genaromazur03@gmail.com>
2022-06-08 12:26:58 -0300

    [add : routes.js || mod : app.js]

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express=require("express")
 const app=express()
 const path=require("path")
+const routes=require("./routes/routes")
+
 const port=process.env.PORT || 8080
 const directory=path.join(__dirname,"./../public")
 
@@ -9,25 +11,4 @@ app.listen(port,()=>{
 })
 
 app.use(express.static(directory))
-
-app.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname,"/views/index.html"))
-})
-app.get("/login",(req,res)=>{
-    res.sendFile(path.join(__dirname,"/views/login.html"))
-})
-app.get("/registro",(req,res)=>{
-    res.sendFile(path.join(__dirname,"/views/register.html"))
-})
-app.get("/CarritoDeCompras",(req,res)=>{
-    res.sendFile(path.join(__dirname,"/views/productCard.html"))
-})
-app.get("/DetalleDeProducto",(req,res)=>{
-    res.sendFile(path.join(__dirname,"/views/productDetail.html"))
-})
-app.get("/contacto",(req,res)=>{
-    res.sendFile(path.join(__dirname,"/views/contacto.html"))
-})
-app.get("/nosotros",(req,res)=>{
-    res.sendFile(path.join(__dirname,"/views/nosotros.html"))
-})
+app.use("/",routes)
