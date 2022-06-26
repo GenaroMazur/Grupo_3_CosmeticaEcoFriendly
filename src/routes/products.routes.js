4652f79753b188b327bda7935d98e9bd17c48d37
Genaro_Mazur <genaromazur03@gmail.com>
2022-06-26 14:18:58 -0300

    [mod products.routes , productscontroller]

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,35 +1,36 @@
-const express=require("express")
-const routes=express.Router()
-const multer=require("multer")
-const path=require("path")
+const express = require("express")
+const routes = express.Router()
+const multer = require("multer")
+const path = require("path")
 
 //Configurar el multer
-const storage=multer.diskStorage({
-    destination:(req,file,cb)=>{
-        let direction=path.join(__dirname,"./../../public/img/products_images")
-        cb(null,direction)
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        let direction = path.join(__dirname, "./../../public/img/products_images")
+        cb(null, direction)
     },
-    filename:(req,file,cb)=>{
-        let filename="pruct_"+productController.usersList().length+path.extname(file.originalname)
-        cb(null,filename)
+    filename: (req, file, cb) => {
+        console.log(req.body)
+        let filename = req.body.category+"_"+ Date.now() + path.extname(file.originalname)
+        cb(null, filename)
     }
 })
-const upload=multer({storage})
+const upload = multer({ storage })
 
 //importa controlador
-const productController=require("./../controllers/productController")
+const productController = require("./../controllers/productController")
 
 //GET
-routes.get("/CarritoDeCompras",productController.productCard)
-routes.get("/DetalleDeProducto",productController.productDetail)
-routes.get("/newProduct",productController.newProduct)
-routes.get("/editProduct",productController.editProduct)
+routes.get("/CarritoDeCompras", productController.productCard)
+routes.get("/DetalleDeProducto", productController.productDetail)
+routes.get("/newProduct", productController.newProduct)
+routes.get("/editProduct", productController.editProduct)
 
 //POST
-routes.post("/newProduct",upload.single("productImg"),productController.createProduct)
+routes.post("/newProduct", upload.single("image"), productController.createProduct)
 
 //PUT
 
 //DELETE
 
-module.exports=routes
+module.exports = routes
