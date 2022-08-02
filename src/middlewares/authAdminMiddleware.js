7f6f38b17c4df88b61a0b1abd9c93f5fabe52b10
Genaro_Mazur <genaromazur03@gmail.com>
2022-08-02 11:38:44 -0300

    [ mod : app.js, auth(admin,Guest,user)Middleware, users.JSON ]

--- src/middlewares/authAdminMiddleware.js
+++ src/middlewares/authAdminMiddleware.js
@@ -1,9 +1,10 @@
 module.exports = function (req, res, next) {
-    if(req.session.user.status == "admin") {
+    if(req.session.user.status == "admin" || process.argv[2]) {
         next()
     } else if(req.session.user.status == "user"){
         res.redirect("/")
     } else {
         res.send("/user/login")
     }
+    
 }
