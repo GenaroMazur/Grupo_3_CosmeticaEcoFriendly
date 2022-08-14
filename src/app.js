6b097c97ef3732792f310df7f7190a4c90f7bbf3
Genaro_Mazur <genaromazur03@gmail.com>
2022-08-14 16:47:03 -0300

    [ add : sessionAppMiddleware.js || mod : app.js]

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
 const express = require("express")
 const app = express()
 const path = require("path")
-const session = require("express-session")
-const cookie = require("cookie-parser")
+const routes = require("./routes/index.routes")
 
 //utiliza la carpeta public
 const directory = path.join(__dirname, "./../public")
@@ -21,41 +20,30 @@ app.set('views', __dirname + '/views')
 app.set("view engine", "ejs")
 
 //express session
+const session = require("express-session")
+const sessionAppMiddleware = require("./middlewares/sessionAppMiddleware")
+
 app.use(session({
     secret:"jabones",
     resave:false,
     saveUninitialized:false
 }))
-app.use((req, res, next)=> {
-    if (process.argv[2] && req.session.user == undefined) {
-        req.session.user = {
-            "id": 1659450540488,
-            "username":"desarrollador",
-            "status":"admin"
-        }
-    }
-    if (req.cookies && req.session.user == undefined) {
-        req.session.user = req.cookies.remember
-    } else if (req.cookies == undefined && req.session.user == undefined) {
-        req.session.user = {status : "guest"}
-    }
-    return next()
-})
+app.use(sessionAppMiddleware)
 
 //Cookie Parser
+const cookie = require("cookie-parser")
 app.use(cookie())
 
 //inicia el puerto de escucha
 const port = process.env.PORT || 8080
 app.listen(port, () => {
     console.log("funcionando en el puerto: " + port)
-    console.log("Corriendo el servidor en modo desarrollo")
+    if(process.argv[2]){
+        console.log("Corriendo el servidor en modo desarrollo")
+    }
 })
 
 //Importa las rutas
-const routes = require("./routes/index.routes")
-const cookieParser = require("cookie-parser")
-const modelsController = require("./models/modelsController")
 app.use("/", routes)
 app.use((req, res, next) => {
     res.status(404).render("not-found")
---
+++ src/middlewares/sessionAppMiddleware.js
@@ -0,0 +1,15 @@
+module.exports=function (req, res, next){
+    if (process.argv[2] && req.session.user == undefined) {
+        req.session.user = {
+            "id": 1659450540488,
+            "username":"desarrollador",
+            "status":"admin"
+        }
+    }
+    if (req.cookies && req.session.user == undefined) {
+        req.session.user = req.cookies.remember
+    } else if (req.cookies == undefined && req.session.user == undefined) {
+        req.session.user = {status : "guest"}
+    }
+    return next()
+}
