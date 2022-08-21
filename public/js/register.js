6ef8f10aaa607f30caf3dfef378ef41a1ee6efe2
FabricioSaucedo <fabriciosaucedo.fs@gmail.com>
2022-08-21 17:01:59 -0300

    [mod: register.js]

--- public/js/register.js
+++ public/js/register.js
@@ -1,29 +1,40 @@
-let ojo = document.querySelector(".eye");
+let ojo = document.querySelectorAll(".eye");
+let password = document.querySelectorAll(".password")
 
-ojo.addEventListener("click", function() {
+ojo[0].addEventListener("click", function() {
     let icon = this.querySelector("ion-icon")
-    if(this.nextElementSibling.type === "password"){
-        this.nextElementSibling.type = "text";
-        icon.setAttribute("name", "eye-outline");
-        icon.setAttribute("name", "eye-off-outline");
-       }else{
-        this.nextElementSibling.type = "password";
-        icon.setAttribute("name", "eye-off-outline");
-        icon.setAttribute("name", "eye-outline");
-       }
+    // if(this.nextElementSibling.type === "password"){
+    //     this.nextElementSibling.type = "text";
+    //     icon.setAttribute("name", "eye-outline");
+    //     icon.setAttribute("name", "eye-off-outline");
+    //    }else{
+    //     this.nextElementSibling.type = "password";
+    //     icon.setAttribute("name", "eye-off-outline");
+    //     icon.setAttribute("name", "eye-outline");
+    //    }
+    if (password[0].type == "password"){
+        password[0].type = "text"
+    }else{
+        password[0].type = "password"
+    }
 });
 
-let ojos = document.querySelector(".eyes");
+// let ojos = document.querySelector(".eyes");
 
-ojos.addEventListener("click", function() {
-    let icon = this.querySelector("ion-icon")
-   if(this.nextElementSibling.type === "password"){
-    this.nextElementSibling.type = "text";
-    icon.setAttribute("name", "eye-outline");
-    icon.setAttribute("name", "eye-off-outline");
-   }else{
-    this.nextElementSibling.type = "password";
-    icon.setAttribute("name", "eye-off-outline");
-    icon.setAttribute("name", "eye-outline");
-   }
+ojo[1].addEventListener("click", function() {
+//     let icon = this.querySelector("ion-icon")
+//    if(this.nextElementSibling.type === "password"){
+//     this.nextElementSibling.type = "text";
+//     icon.setAttribute("name", "eye-outline");
+//     icon.setAttribute("name", "eye-off-outline");
+//    }else{
+//     this.nextElementSibling.type = "password";
+//     icon.setAttribute("name", "eye-off-outline");
+//     icon.setAttribute("name", "eye-outline");
+//    }
+   if (password[1].type == "password"){
+    password[1].type = "text"
+}else{
+    password[1].type = "password"
+}
 });
