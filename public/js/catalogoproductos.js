cd850b476b090c0836bb8637c13ed643a8649198
Leandrojoelramos <leandrojoelramos7@gmail.com>
2022-08-22 19:37:16 -0300

    [mod : root.style.css]

--- public/js/catalogoproductos.js
+++ public/js/catalogoproductos.js
@@ -2,7 +2,6 @@ let corazon = document.querySelector(".boton.heartBoton");
 
 corazon.addEventListener("click", function() {
 let icono = this.querySelector("ion-icon");    
-    icono.setAttribute("name", "heart-outline");
     icono.setAttribute("name", "heart");
     myP = document.querySelector ("ion-icon.heartbuton.md.hydrated");
     myP.style.color = "hotpink";
