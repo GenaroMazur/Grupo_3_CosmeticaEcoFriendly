b260cac9b8287f9745bfdff6690a8275ec01e3ea
Leandrojoelramos <leandrojoelramos7@gmail.com>
2022-08-24 21:13:51 -0300

    [add : carousel.js]

--- public/js/catalogoproductos.js
+++ public/js/catalogoproductos.js
@@ -1,8 +1,25 @@
+//icono de corazon
 let corazon = document.querySelector(".boton.heartBoton");
 
-corazon.addEventListener("click", function() {
-let icono = this.querySelector("ion-icon");    
+corazon.addEventListener("click", function () {
+    let icono = this.querySelector("ion-icon");
     icono.setAttribute("name", "heart");
-    myP = document.querySelector ("ion-icon.heartbuton.md.hydrated");
+    myP = document.querySelector("ion-icon.heartbuton.md.hydrated");
     myP.style.color = "hotpink";
 });
+//carousel de produtos
+let flecha = document.querySelector(".flecha");
+let caja = document.querySelector(".caja");
+let posicion = 2;
+let valor = -15.52;
+
+let multiplicacion = posicion * valor;
+flecha.addEventListener("click", function () {
+    caja.style.transform = `translateX(${multiplicacion}vw)`
+});
+
+caja.addEventListener("click", function () {
+    caja.style.transform = `translateX(${valor}vw)`;
+});
+
+
