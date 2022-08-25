cd841320d300585b3ebb8eee22030085d4cb678e
Leandrojoelramos <leandrojoelramos7@gmail.com>
2022-08-25 03:17:38 -0300

    [fix : carousel.js]

--- public/js/catalogoproductos.js
+++ public/js/catalogoproductos.js
@@ -10,22 +10,85 @@ corazon.addEventListener("click", function () {
 
 //carousel de produtos
 let flecha = document.querySelectorAll(".flecha");
-let caja = document.querySelector(".caja");
+let caja = document.querySelectorAll(".caja");
 let productos = document.querySelectorAll(".productos");
 
-function next(){
-    caja.style.transform  = `translateX(-15.52vw)`;
-    caja.style.transition = "all 0.5s";
+//shampoo
+function nextA(){
+
+    caja[0].style.transform  = `translateX(-77.5vw)`;
+    caja[0].style.transition = "all 0.5s";
 }
+
 flecha[1].addEventListener("click", function () {
-next()
+nextA()
 });
-function prev(){
-    caja.style.transform  = `translateX(0vw)`;
-    caja.style.transition = "all 0.5s";
+
+function prevA(){
+    caja[0].style.transform  = `translateX(0vw)`;
+    caja[0].style.transition = "all 0.5s";
 }
+
 flecha[0].addEventListener("click", function () {
-    prev()
+    prevA()
+});
+//acondicionador
+function nextB(){
+
+    caja[1].style.transform  = `translateX(-77.5vw)`;
+    caja[1].style.transition = "all 0.5s";
+}
+
+flecha[3].addEventListener("click", function () {
+nextB()
+});
+
+function prevB(){
+    caja[1].style.transform  = `translateX(0vw)`;
+    caja[1].style.transition = "all 0.5s";
+}
+
+flecha[2].addEventListener("click", function () {
+    prevB()
+});
+//jabon
+function nextC(){
+
+    caja[2].style.transform  = `translateX(-77.5vw)`;
+    caja[2].style.transition = "all 0.5s";
+}
+
+flecha[5].addEventListener("click", function () {
+nextC()
+});
+
+function prevC(){
+    caja[2].style.transform  = `translateX(0vw)`;
+    caja[2].style.transition = "all 0.5s";
+}
+
+flecha[4].addEventListener("click", function () {
+    prevC()
+});
+
+//desodorante
+function nextD(){
+
+    caja[3].style.transform  = `translateX(-77.5vw)`;
+    caja[3].style.transition = "all 0.5s";
+}
+
+flecha[7].addEventListener("click", function () {
+nextD()
+});
+
+function prevD(){
+    caja[3].style.transform  = `translateX(0vw)`;
+    caja[3].style.transition = "all 0.5s";
+}
+
+flecha[6].addEventListener("click", function () {
+    prevD()
 });
 
 /*let valor = "-15.52";
