3cde3bada4761c92f4af28a736f23fce138f09f8
FabricioSaucedo <fabriciosaucedo.fs@gmail.com>
2022-09-03 19:19:14 -0300

    [add: ValidatorNewProduct.js, mod: structure.sql]

--- public/js/ValidatorNewProduct.js
+++ public/js/ValidatorNewProduct.js
@@ -2,6 +2,28 @@ window.addEventListener('load', function(){
     let formCreateProduct = document.querySelector('form');
     formCreateProduct.addEventListener('submit', function(){
 
-        this
+        inputName = document.getElementById('nameProduct');
+        inputDescription = document.getElementById('description');
+        inputCategory = document.getElementById('Category');
+        inputImage = document.getElementById('image');
+        inputFragrance = document.getElementById('fragrance');
+        inputIngredients = document.getElementById('ingredients');
+        inputModeOfUse = document.getElementById('modeOfUser');
+        inputGrams = document.getElementById('grams');
+        inputPrice = document.getElementById('price');
+
+        if( inputName.value == '') {
+
+        }
+
+        if(inputName.value != '') {
+
+        } else if( inputName.value.length > 5) {
+
+            
+
+        }
+
+
     })
 })
