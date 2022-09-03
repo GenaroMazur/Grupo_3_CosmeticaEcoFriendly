fdb1eac1184b3b5890a7fba1fd1078846059d724
FabricioSaucedo <fabriciosaucedo.fs@gmail.com>
2022-09-03 16:41:29 -0300

    [mod: validacion.js, register.ejs]

---
+++ public/js/ValidatorNewProduct.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function(){
+    let formCreateProduct = document.querySelector('form');
+    formCreateProduct.addEventListener('submit', function(){
+
+        this
+    })
+})
