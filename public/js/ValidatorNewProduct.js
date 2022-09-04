b72b68ebd2b106d85d81868f62745e48d7869a9c
FabricioSaucedo <fabriciosaucedo.fs@gmail.com>
2022-09-04 14:53:53 -0300

    [mod: ValidatorNewProduct.js]

--- public/js/ValidatorNewProduct.js
+++ public/js/ValidatorNewProduct.js
@@ -1,29 +1,81 @@
-window.addEventListener('load', function(){
-    let formCreateProduct = document.querySelector('form');
-    formCreateProduct.addEventListener('submit', function(){
 
-        inputName = document.getElementById('nameProduct');
-        inputDescription = document.getElementById('description');
-        inputCategory = document.getElementById('Category');
-        inputImage = document.getElementById('image');
-        inputFragrance = document.getElementById('fragrance');
-        inputIngredients = document.getElementById('ingredients');
-        inputModeOfUse = document.getElementById('modeOfUser');
-        inputGrams = document.getElementById('grams');
-        inputPrice = document.getElementById('price');
+    let formCreateProduct = document.getElementById('form_create');
 
-        if( inputName.value == '') {
+    let inputName = document.getElementById('nameProduct');
+    let inputDescription = document.getElementById('description');
+    let inputCategory = document.getElementById('Category');
+    let inputImage = document.getElementById('image');
+    let inputFragrance = document.getElementById('fragrance');
+    let inputIngredients = document.getElementById('ingredients');
+    let inputModeOfUse = document.getElementById('modeOfUser');
+    let inputGrams = document.getElementById('grams');
+    let inputPrice = document.getElementById('price');
+
+  
+
+    let arrayInput = [inputName, inputImage, inputIngredients, inputModeOfUse, inputPrice];
+    let textArea = inputDescription;
+    let arraySelect = [inputCategory, inputFragrance, inputGrams];
+
+
+    formCreateProduct.addEventListener('submit', function(e){
+
+        let errores = 0;
+        
+        arrayInput.forEach(function(input){
+            input.nextElementSibling.innerHTML = ''
+            if(input.value.length == 0){
+                input.classList.add('is-invalid');
+                errores++;
+                input.nextElementSibling.innerHTML = 'El campo no puede estar vacío'
+                return;
+            }
+
+            if(input.value != ''){
+                input.classList.remove('is-invalid');
+                input.nextElementSibling.innerHTML = ''
+            }
+
+        });
+
+        //aca evaluar la descripcion
+        inputDescription.addEventListener('blur', function(){
+            if(inputDescription.value < 20){
+                inputDescription.classList.add('is-invalid');
+                inputDescription.nextElementSibling.innerHTML = 'Debe contener al menos 20 caracteres la descripción'
+
+            }
+        })
+        arraySelect.forEach(function(select){
+            //aca evaluar los select
+            if(select.value == ''){
+
+            }
+            if(select.value != ''){
+
+            }
+        })
+
+        if(errores > 0){
+            e.preventDefault();
 
         }
-
-        if(inputName.value != '') {
-
-        } else if( inputName.value.length > 5) {
-
-            
-
-        }
-
-
+        
     })
-})
+
+    arrayInput.forEach(function(input){
+        input.addEventListener('blur', function(){
+            if(input.value == ''){
+                input.classList.add('is-invalid');
+                input.classList.remove('is-valid');
+                input.nextElementSibling.innerHTML = 'El campo no puede estar vacío'
+            }
+
+            if(input.value != ''){
+                input.classList.add('is-valid');
+                input.classList.remove('is-invalid');
+                input.nextElementSibling.innerHTML = ''
+            }
+
+        })
+    })
