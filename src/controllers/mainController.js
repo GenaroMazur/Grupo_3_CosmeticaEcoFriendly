cbd55948f514f880d24e312c3b0d2d497a1dee60
genaro Mazur <genaromazur03@gmail.com>
2022-09-13 00:02:42 -0300

    [ mod : mainController.js]

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,33 +1,33 @@
+const db = require("./../database/models");
 
-const db = require("./../database/models")
 const mainController = {
 
      //pagina de inicio
      index: function (req, res) {
-          let user = req.session.user
+          let user = req.session.user;
           db.Product.findAll()
                .then(products => {
-                    res.render("index", { products, user })
+                    res.render("index", { products, user });
                })
                .catch(err => {
 
                     console.log(err);
-                    res.render("maintain", { user })
-               })
+                    res.render("maintain", { user });
+               });
      },
 
      //pagina de nosotros
      nosotros: (req, res) => {
-          let status = req.session.user
-          let user = status
-          return res.render('nosotros', { status, user })
+          let status = req.session.user;
+          let user = status;
+          return res.render('nosotros', { status, user });
      },
 
      //pagina de contacto
      contacto: (req, res) => {
-          let status = req.session.user
-          let user = status
-          return res.render('contacto', { status, user })
+          let status = req.session.user;
+          let user = status;
+          return res.render('contacto', { status, user });
      },
 
 }
