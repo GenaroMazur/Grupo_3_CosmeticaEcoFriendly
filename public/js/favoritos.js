0dcab7215cb5ae815229d8c5fe9af738a3740473
Leandrojoelramos <leandrojoelramos7@gmail.com>
2022-09-28 07:35:36 -0300

    [mod : favoritos.all]

--- public/js/favoritos.js
+++ public/js/favoritos.js
@@ -14,3 +14,10 @@ mas.forEach((element,index)=>{
     })
 })
 
+document.querySelectorAll('.button').forEach(button => button.addEventListener('click', e => {
+    if(!button.classList.contains('loading')) {
+    button.classList.add('loading');
+    setTimeout(() => button.classList.remove('loading'), 3700);
+    }
+    e.preventDefault();
+    }));
